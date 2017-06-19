b5668a8cdb775309f97b061833cebacffde4c2ce
r-korchagin <r-korchagin@yandex.ru>
2017-06-19 21:29:36 +0300

    Update searchFormView.js

--- static/js/searchFormView.js
+++ static/js/searchFormView.js
@@ -80,7 +80,7 @@ Function return html for empty searching result
 searchEmptyResultView : function() {
    return "\
             <p class=\"content-subtitle\">Can not find anything by your request</p>\
-            <p class=\"content-subtitle\">Try to find again</p>\
+            <p class=\"content-subtitle\">Please, try to change the search parameters.</p>\
    "; 
 },
 
